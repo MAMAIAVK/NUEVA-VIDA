7d86bd8e07405f90786143ce516e15c2d9346a8a
MAMAIAVK <isahoyo@gmail.com>
2024-11-15 13:10:15 +0100

    java

---
+++ script.js
@@ -0,0 +1,16 @@
+/*alert("hola mundo");
+alert(12 * 4 + 3 - 21);*/
+/*comentario de bloque*/
+//comentario de linea//
+
+
+let saludo = "holaaaaaaa";
+
+let despedida;
+despedida = "chaito";
+saludo = "que pasaaaaaa"
+let nombreUsuario = "MAMAIAVK";
+
+alert(saludo + nombreUsuario);
+
+saludo = despedida
